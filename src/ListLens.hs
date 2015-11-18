cd7276463bcc71955664785ecf0f9f1f603548ac
Joachim Breitner <mail@joachim-breitner.de>
2015-11-18 14:27:49 +0100

    More simplification

--- src/ListLens.hs
+++ src/ListLens.hs
@@ -6,15 +6,15 @@ module ListLens where
 import Order (Order, permute, OrderKey)
 import Report (ReportElemID)
 
-type ReportElems = Order ReportElemID ()
+type ReportElems = Order ReportElemID
 
 -- this indirection is needed
 listReorder :: ReportElemID ->  ReportElems -> ReportElems
 listReorder = listReorder''
 
 -- Type signature is needed
-listReorder'' :: forall k v. (Enum k, OrderKey k) =>
-                 k -> Order k v -> ReportElems
+listReorder'' :: forall k. (Enum k, OrderKey k) =>
+                 k -> Order k -> ReportElems
 listReorder'' ps order = reorder
     where
       -- This indirection is needed
--- src/Order.hs
+++ src/Order.hs
@@ -10,21 +10,16 @@ class Ord k => OrderKey k where
     init :: k      -- ^ The initial key value (toEnum 0?)
 
 class OrderKey k => OrderMap k where
-    data Order k :: * -> *
+    data Order k :: *
 
-    insert :: Order k v
+    insert :: Order k
     insert = Order init
 
-    permute :: k -> Order k v -> ()
-    permute neworder (Order k) = sanitize `seq` error "done"
-      where
-        sanitize = neworder == k
+    permute :: k -> Order k -> ()
+    permute k' (Order k) = k' == k `seq` error "done"
 
--- | Given any instance of OrderKey, we can create an instance of
--- OrderMap which includes an Order type containing a Map, a list
--- of keys, and a field to hold the next available key.
 instance OrderKey k => OrderMap k where
-    data Order k v = Order  k
+    data Order k = Order k
 
 instance (Ord a, Enum a) => OrderKey a where
     init = toEnum 1            -- Yeah, that's right, 1.  F**k zeroth elements.
